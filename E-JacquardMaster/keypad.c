e6b2fdbb70dec6218552fdc8f54ad0921be747e7
Jayaram <jayaram220@gmail.com>
2020-04-20 15:58:32 +0530

    keypad_file@Development_branch

--- E-JacquardMaster/keypad.c
+++ E-JacquardMaster/keypad.c
@@ -1,27 +1,79 @@
+
+/**
+  ******************************************************************************
+  * @file    keypad.c
+  * @author  Devsys embedded team
+  * @version V1.1.0
+  * @date    18-April-2020
+  * @brief   This file provides keypad functions to manage the following 
+  *          functionalities of the keypad:
+  *           + 
+  *           + 
+  *
+ 
+  ******************************************************************************
+  */
+
+
+
+
+/* Includes ------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 #include "display.h"
 #include "usb.h"
 
-#define E_KEY_MAIN  	0x02
-#define E_KEY_ENTER  	0x04
-#define E_KEY_UP	0x08
-#define E_KEY_DOWN  	0x10
-#define E_KEY_PALLU  	0x12
-#define E_KEY_BODY  	0x14
+#define E_KEY_MAIN  	0x02  // Defining MAIN key
+#define E_KEY_ENTER  	0x04  // Defining ENTER key
+#define E_KEY_UP	0x08  // Defining UP key
+#define E_KEY_DOWN  	0x10  // Defining DOWN key
+#define E_KEY_PALLU  	0x12  // Defining BORDER key
+#define E_KEY_BODY  	0x14  // Defining BODY key
 
 
-#define KEY_NUMBER_0	0x40
-#define KEY_NUMBER_9	0x49
-static unsigned int gsKeyNumber = 1;
-static unsigned int gsKeyIndex = 0;
+#define KEY_NUMBER_0	0x40 // Defining KEY_NUMBER_0 as hex value
+#define KEY_NUMBER_9	0x49 // Defining KEY_NUMBER_9 as hex value
 
-#define RESET_KEY_NUMBERS	gsKeyNumber = 1; gsKeyIndex = 0;
+static unsigned int gsKeyNumber = 1; /* Declaring a positive integer of static to preserves it's previous value */
+static unsigned int gsKeyIndex = 0;  /* Declaring a positive integer of static to preserves it's previous value */
+ 
+#define RESET_KEY_NUMBERS	gsKeyNumber = 1; gsKeyIndex = 0;// doubt this syntax
+
+
+/**
+  * @brief  Configures Keypad, when ever user press perticular key that corresponding operation will perform.
+  * @note   If the current state is EJACQUARD_STATE_RUNNING and if the key pressed is MAIN key- 
+  *         The state will change to idle state. 
+  *        
+  * @note   Validate each and every key pressed by user inside switch. 
+  *         If the switch key is E_KEY_MAIN, ie pressed by MAIN key, mechine state will change to Running state.
+  *         After that,the reset key reset by RESET_KEY_NUMBERS.
+
+  * @note   If Enter key is pressed, check whether the state is Usb_config, if it is Usb_config.
+
+  * @note   copy the image inside the usb stick into device else will print messege then Reset the Resetkey.
+  *         usb_select_file() will select the file in /dev/ttyusb device file.
+
+  * @note   If Enter key is pressed, check whether the state is Usb_config, if it is Usb_config 	
+  *	    copy the image inside the usb stick into device else will print messege then Reset the Resetkey. 
+
+  * @note   If Enter UP key is pressed scroll up the 16*2 display screen Increment the  sgUsbFileDisplayIndex and call the display-1 screen. 
+  * 	    In usb mode this Up key will move the usb file list up, when GET_STATE() becomes EJACQUARD_STATE_USB_CONFIG.	
+  *	    In usb mode this Down key Decrement the displayList[] and call the display-1 screen, when GET_STATE() becomes 
+  *         EJACQUARD_STATE_USB_CONFIG.       
+  * @param  UINT8_T key: charector key.
+  * @retval None
+  */
+
+
 void keypad_handler(UINT8_T key)
 {
 	/* Do Nothing during the RUNNING STATE */
-	if (GET_STATE() == EJACQUARD_STATE_RUNNING)  {
+
+	if (GET_STATE() == EJACQUARD_STATE_RUNNING)  
+	{
 		if ( key = E_KEY_MAIN ) CHANGE_STATE(EJACQUARD_STATE_IDLE);
 		return;
 	}
@@ -39,7 +91,6 @@ void keypad_handler(UINT8_T key)
 			RESET_KEY_NUMBERS;
 			break;
 		case E_KEY_UP:
-			/* In usb mode this up key will move the usb file list up */
 			if (GET_STATE() == EJACQUARD_STATE_USB_CONFIG)
 				usb_scroll_up();
 			else 
@@ -48,7 +99,6 @@ void keypad_handler(UINT8_T key)
 			RESET_KEY_NUMBERS;
 			break;
 		case E_KEY_DOWN:
-			/* In usb mode this up key will move the usb file list up */
 			if (GET_STATE() == EJACQUARD_STATE_USB_CONFIG)
 				usb_scroll_down();		
 			else 
@@ -76,6 +126,14 @@ void keypad_handler(UINT8_T key)
 	}
 	return;	
 }
+
+/**
+  * @brief  initialize the keypad_handler into the keypad intrrupt line.
+  * @note   Initialize UP,DOWN,MAIN keys.
+  * @param  No parameter.
+  * @retval zeo
+  */
+
 
 
 int keypad_init(void)
